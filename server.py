a8f237b0e0d6edfe27d75a2abbe9f67c412433e7
moamei18 <moameia353@gmail.com>
2026-04-26 12:40:54 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, render_template_string
 
 app = Flask(__name__)
@@ -99,4 +100,5 @@ def home():
     return render_template_string(HTML, mode=mode, time=time, name=name, avatar=avatar, squares=squares)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
